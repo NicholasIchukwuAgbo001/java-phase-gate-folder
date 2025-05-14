5c26b2e06364ddb6cac9969e74959057bd8f0da3
NicholasIchukwuAgbo001 <nicholasichukwuagbo@gmail.com>
2025-05-14 10:10:42 +0100

    update estate app

--- SecondPhaseGate3in1java/src/estateApp/model/AccessCode.java
+++ SecondPhaseGate3in1java/src/estateApp/model/AccessCode.java
@@ -1,4 +1,5 @@
 package estateApp.model;
 
 public class AccessCode {
+
 }
--- SecondPhaseGate3in1java/src/estateApp/model/Resident.java
+++ SecondPhaseGate3in1java/src/estateApp/model/Resident.java
@@ -1,4 +1,7 @@
 package estateApp.model;
 
 public class Resident {
+    private int id;
+    private String name;
+    private String address;
 }
